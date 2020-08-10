4ddcd4a32f4cc2ac3616a60dda98de32806f85b8
JacksonReynolds <jwreynod@gmail.com>
2020-08-10 13:39:23 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+
+    def actors_list
+        self.characters.collect {|character| "#{character.actor.first_name} #{character.actor.last_name}"}
+    end
+
+    def build_network(call_letters:)
+        network = Network.new(call_letters: call_letters)
+        self.network = network
+        network
+    end
 end
