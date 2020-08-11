afae8d2be7a76e456ed2237b4f6733901807f2f6
Ethan <eleetyson@gmail.com>
2020-08-11 10:50:38 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,8 +2,14 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 
+# returns a string with the first and last name of the current actor
   def full_name
-    #test
+    self.first_name + " " + self.last_name
+  end
+
+# returns an array of all roles an actor has played
+  def list_roles
+    self.characters.map { |c| "#{c.name} - #{c.show.name}" }
   end
 
 end
